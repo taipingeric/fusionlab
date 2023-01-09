adc6d9d15c852b3ba3207e3e21e827995d2828e2
CYLi <taipingeric@gmail.com>
2023-01-10 03:44:39 +0800

    add TFSegmentationModel bump version

--- fusionlab/segmentation/base.py
+++ fusionlab/segmentation/base.py
@@ -1,11 +1,24 @@
 import torch.nn as nn
+from tensorflow.keras import Model
 
 
 class SegmentationModel(nn.Module):
     """
-    Base class of the segmentation model with Encoder, Bridger, Decoder, Head
+    Base PyTorch class of the segmentation model with Encoder, Bridger, Decoder, Head
     """
     def forward(self, x):
+        features = self.encoder(x)
+        feature_fusion = self.bridger(features)
+        decoder_output = self.decoder(feature_fusion)
+        output = self.head(decoder_output)
+        return output
+
+
+class TFSegmentationModel(Model):
+    """
+    Base PyTorch class of the segmentation model with Encoder, Bridger, Decoder, Head
+    """
+    def call(self, x):
         features = self.encoder(x)
         feature_fusion = self.bridger(features)
         decoder_output = self.decoder(feature_fusion)
