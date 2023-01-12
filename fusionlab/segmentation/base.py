a465612808c12e66abda0e6520c195e480e6886b
CYLi <taipingeric@gmail.com>
2023-01-12 15:54:01 +0800

    update comment

--- fusionlab/segmentation/base.py
+++ fusionlab/segmentation/base.py
@@ -19,6 +19,16 @@ class TFSegmentationModel(Model):
     Base PyTorch class of the segmentation model with Encoder, Bridger, Decoder, Head
     """
     def call(self, x, training=None):
+        """
+
+        Args:
+            x: input tensor
+            training: flag for BatchNormalization and Dropout, whether the layer should behave in training mode or in
+            inference mode
+
+        Returns:
+
+        """
         features = self.encoder(x, training)
         feature_fusion = self.bridger(features, training)
         decoder_output = self.decoder(feature_fusion, training)
