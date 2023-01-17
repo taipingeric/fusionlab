0cbdb206c2e729e68af1df61d3cd059cffed342f
EricLi <taipingeric@gmail.com>
2023-01-17 19:28:28 +0800

    update torch dice

--- fusionlab/losses/dice/dice.py
+++ fusionlab/losses/dice/dice.py
@@ -11,7 +11,7 @@ MULTICLASS_MODE = "multiclass"
 class DiceCE(nn.Module):
     def __init__(self, w_dice=0.5, w_ce=0.5, cls_weight=None):
         """
-
+        Dice Loss + Cross Entropy Loss
         Args:
             w_dice: weight of Dice Loss
             w_ce: weight of CrossEntropy loss
@@ -33,7 +33,7 @@ class DiceCE(nn.Module):
 class DiceLoss(nn.Module):
     def __init__(
         self,
-        mode="multiclass",  # binary
+        mode="multiclass",  # binary, multiclass
         log_loss=False,
         from_logits=True,
     ):
@@ -60,7 +60,7 @@ class DiceLoss(nn.Module):
         assert y_true.size(0) == y_pred.size(0)
         bs = y_true.size(0)
         num_classes = y_pred.size(1)
-        dims = (0, 2) # # (N, -1, H*W)
+        dims = (0, 2)  # (N, -1, H*W)
 
         if self.from_logits:
             # get [0..1] class probabilities
@@ -73,17 +73,16 @@ class DiceLoss(nn.Module):
             y_true = y_true.view(bs, 1, -1)
             y_pred = y_pred.view(bs, 1, -1)
         elif self.mode == MULTICLASS_MODE:
-            y_true = y_true.view(bs, -1)
-            y_pred = y_pred.view(bs, num_classes, -1)
+            y_pred = y_pred.view(bs, num_classes, -1) # N, C, HW
 
-            y_true = F.one_hot(y_true, num_classes)  # N, H*W -> N,H*W, C
-            y_true = y_true.permute(0, 2, 1)  # H, C, H*W
+            y_true = y_true.view(bs, -1)
+            y_true = F.one_hot(y_true, num_classes)  # N, HW -> N, HW, C
+            y_true = y_true.permute(0, 2, 1)  # N, C, HW
         else:
             AssertionError("Not implemented")
-
         scores = soft_dice_score(y_pred, y_true.type_as(y_pred), dims=dims)
         if self.log_loss:
-            loss = -torch.log(scores.clamp_min(self.eps))
+            loss = -torch.log(scores.clamp_min(1e-7))
         else:
             loss = 1.0 - scores
         return loss.mean()
@@ -92,8 +91,8 @@ class DiceLoss(nn.Module):
 def soft_dice_score(pred, target, dims=None):
     """
     Shape:
-        - Input: :math:`(N, C, *)` where :math:`*` means any number of additional dimensions
-        - Target: :math:`(N, C, *)`, same shape as the input
+        - Input: :math:`(N, C, *)`
+        - Target: :math:`(N, C, *)`
         - Output: scalar.
     """
     assert pred.size() == target.size()
@@ -105,23 +104,31 @@ def soft_dice_score(pred, target, dims=None):
 
 
 if __name__ == "__main__":
+
+    print("multiclass")
     pred = torch.tensor([[
-        [1., 2., 3.],
-        [2., 6., 4.],
-        [9., 6., 3.],
-        [4., 8., 12.]
-    ]]).permute(0, 2, 1)
-    true = torch.tensor([[2, 1, 0, 2]])
-    print(pred.shape, true.shape)
+        [1., 2., 3., 4.],
+        [2., 6., 4., 4.],
+        [9., 6., 3., 4.]
+    ]]).view(1, 3, 4)
+    true = torch.tensor([[2, 1, 0, 2]]).view(1, 4)
+
     dice = DiceLoss("multiclass", from_logits=True)
     loss = dice(pred, true)
-    print(loss, '== 0.1350?')
+    assert loss.item() == 0.5519775748252869
+    print(loss.item()) # 0.5519775748252869
 
     print("Binary")
     pred = torch.tensor([0.4, 0.2, 0.3, 0.5]).reshape(1, 1, 2, 2)
     true = torch.tensor([0, 1, 0, 1]).reshape(1, 2, 2)
-    print(pred.shape, true.shape)
     dice = DiceLoss("binary", from_logits=True)
     loss = dice(pred, true)
-    print(loss, "== 0.4604?")
+    print(loss.item()) #0.46044689416885376
+
+    print("Binary Logloss")
+    pred = torch.tensor([0.4, 0.2, 0.3, 0.5]).reshape(1, 1, 2, 2)
+    true = torch.tensor([0, 1, 0, 1]).reshape(1, 2, 2)
+    dice = DiceLoss("binary", from_logits=True, log_loss=True)
+    loss = dice(pred, true)
+    print(loss.item()) #0.6170140504837036
 
