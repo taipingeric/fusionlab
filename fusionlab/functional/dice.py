9a76cb7a68a6994003fd87c95450ec2ca5f08801
CYLi <taipingeric@gmail.com>
2023-01-31 10:47:35 +0800

    refactor dice

--- fusionlab/functional/dice.py
+++ fusionlab/functional/dice.py
@@ -1,5 +1,6 @@
 import torch
 import tensorflow as tf
+from fusionlab.functional import eps
 
 
 def dice_score(pred, target, dims=None):
@@ -10,11 +11,9 @@ def dice_score(pred, target, dims=None):
         - Output: scalar.
     """
     assert pred.size() == target.size()
-    eps = 1e-7
     intersection = torch.sum(pred * target, dim=dims)
     cardinality = torch.sum(pred + target, dim=dims)
-    dice_score = (2.0 * intersection) / cardinality.clamp(min=eps)
-    return dice_score
+    return (2.0 * intersection) / cardinality.clamp(min=eps)
 
 
 def tf_dice_score(pred, target, axis=None):
@@ -24,10 +23,8 @@ def tf_dice_score(pred, target, axis=None):
         - target: :math:`(N, *, C)`, same shape as the input
         - Output: scalar.
     """
-    eps = 1e-7
     intersection = tf.reduce_sum(pred * target, axis=axis)
     cardinality = tf.reduce_sum(pred + target, axis=axis)
-    dice_score = (2.0 * intersection) / tf.clip_by_value(cardinality,
-                                                         clip_value_min=eps,
-                                                         clip_value_max=cardinality.dtype.max)
-    return dice_score
+    return (2.0 * intersection) / tf.clip_by_value(cardinality,
+                                                   clip_value_min=eps,
+                                                   clip_value_max=cardinality.dtype.max)
