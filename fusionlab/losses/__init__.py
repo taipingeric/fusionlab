6ab8cf5ce8cde338048d4e110482ec418ad2a4d3
EricLi <taipingeric@gmail.com>
2023-02-17 15:08:19 +0800

    update readme add import tversky loss

--- fusionlab/losses/__init__.py
+++ fusionlab/losses/__init__.py
@@ -2,4 +2,6 @@ from .diceloss.dice import DiceLoss, DiceCELoss
 from .diceloss.tfdice import TFDiceLoss
 from .iouloss.iou import IoULoss
 from .iouloss.tfiou import TFIoULoss
+from .tversky.tversky import TverskyLoss
+from .tversky.tftversky import TFTverskyLoss
 
