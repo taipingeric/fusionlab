c458dfa5d4dd7040364f6d3f885b5124320cd86b
ShuYuHuang <b123767195@gmail.com>
2023-05-17 11:16:25 +0800

    add backend switch fro layers and loss

--- fusionlab/layers/__init__.py
+++ fusionlab/layers/__init__.py
@@ -1,1 +1,7 @@
-from .squeeze_excitation import SEModule, TFSEModule
+from fusionlab.configs import BACKEND
+if BACKEND == 'torch':
+    from .squeeze_excitation import SEModule
+elif BACKEND == 'tf':
+    from .squeeze_excitation import TFSEModule
+else:
+    print('backend not supported!!!')
