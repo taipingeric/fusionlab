5933867d7dd974132f19de1cf68beb92b055c11d
EricLi <taipingeric@gmail.com>
2023-05-20 15:25:51 +0800

    refactor SE

--- fusionlab/layers/squeeze_excitation/se.py
+++ fusionlab/layers/squeeze_excitation/se.py
@@ -1,26 +1,33 @@
 import torch
 import torch.nn as nn
-from fusionlab.layers.factories import Conv
+from fusionlab.layers.factories import ConvND
 
 class SEModule(nn.Module):
     def __init__(self, cin, ratio=16, spatial_dims=2):
         super().__init__()
         cout = int(cin / ratio)
         self.gate = nn.Sequential(
-            Conv[spatial_dims](cin, cout, kernel_size=1),
+            ConvND(spatial_dims, cin, cout, kernel_size=1),
             nn.ReLU(inplace=True),
-            Conv[spatial_dims](cout, cin, kernel_size=1),
+            ConvND(spatial_dims, cout, cin, kernel_size=1),
             nn.Sigmoid(),
         )
+        self.spatial_dims = spatial_dims
 
     def forward(self, inputs):
-        x = inputs.mean((-2, -1), keepdim=True)
+        mean_dims = tuple(range(2, 2 + self.spatial_dims))
+        x = inputs.mean(mean_dims, keepdim=True)
         x = self.gate(x)
         return inputs * x
 
 
 if __name__ == '__main__':
-    inputs = torch.normal(0, 1, (1, 256, 224, 224))
+    inputs = torch.normal(0, 1, (1, 256, 16, 16))
     layer = SEModule(256)
     outputs = layer(inputs)
-    assert list(outputs.shape) == [1, 256, 224, 224]
+    assert list(outputs.shape) == [1, 256, 16, 16]
+
+    inputs = torch.normal(0, 1, (1, 256, 16, 16, 16))
+    layer = SEModule(256, spatial_dims=3)
+    outputs = layer(inputs)
+    assert list(outputs.shape) == [1, 256, 16, 16, 16]
