ee449be4bafcd0d156a5130a8e2ad3d40b841b28
ShuYuHuang <b123767195@gmail.com>
2023-06-03 15:32:27 +0800

    add genral torch-to-hf segmentation model wrapper

--- fusionlab/classification/base.py
+++ fusionlab/classification/base.py
@@ -22,3 +22,38 @@ class RNNClassification(nn.Module):
         features, _ = self.encoder(x) # RNN will output feature and states
         output = self.head(features[:, -1, :])
         return output
+
+class HFClassification(nn.Module):
+    """
+    Base Hugginface-pytoch model wrapper class of the classification model
+    """
+    def __init__(self, model,
+                 num_cls=4,
+                 loss_fct=nn.CrossEntropyLoss()):
+        self.net = model
+        self.num_cls = num_cls
+        self.loss_fct = loss_fct
+    def forward(self, x, labels=None):
+        logits = self.net(x)  # Forward pass the model
+        if labels is not None:
+            # logits => [BATCH, NUM_CLS]
+            # labels => [BATCH]
+            loss = self.loss_fct(logits.view(-1, self.num_cls), labels.view(-1))  # Calculate loss
+        else:
+            loss = None
+        # return dictionary for hugginface trainer
+        return {'loss':loss, 'logits':logits, 'hidden_states':None}
+
+# Test the function
+if __name__ == '__main__':
+    import torch
+    from fusionlab.classification import VGG16Classifier
+    H = W = 224
+    cout = 5
+    inputs = torch.normal(0, 1, (1, 3, H, W))
+
+    # Test CNNClassification
+    model = VGG16Classifier(spatial_dims=3, 3, cout, 64)
+    hf_model = HFSegmentationModel(model, cout)
+    output = hf_model(inputs)
+    assert list(output.keys()) == ['loss', 'logits', 'hidden_states']
