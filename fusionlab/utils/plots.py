a5122e569ffb8971674b4098d575241065dcc4de
CYLi <taipingeric@gmail.com>
2023-07-01 09:59:49 +0800

    [Add] test plot channels [Update] add args for controlling show fig

--- fusionlab/utils/plots.py
+++ fusionlab/utils/plots.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_channels(signals):
+def plot_channels(signals, show=True):
     '''
     plot signals by channels
 
@@ -8,7 +8,9 @@ def plot_channels(signals):
         signals: numpy array, shape (num_samples, num_channels)
     '''
     num_channels = signals.shape[1]
-    _, axes = plt.subplots(num_channels, 1, figsize=(10, 10))
+    fig, axes = plt.subplots(num_channels, 1, figsize=(10, 10))
     for i in range(num_channels):
         axes[i].plot(signals[:, i])
-    plt.show()
+    
+    if show: plt.show()
+    return fig
