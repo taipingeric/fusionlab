62f626068ae063c07ae062375df42256a8eab3ad
CYLi <taipingeric@gmail.com>
2023-07-07 16:55:08 +0800

    [Test] SEModule

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -17,3 +17,17 @@ class TestConvNormAct:
         l = ConvNormAct(3, 3, 4, 3, 1, 1)
         outputs = l(inputs)
         assert outputs.shape == torch.Size([1, 4, 16, 16, 16])
+
+class TestSqueezeExcitation:
+    def test_se(self):
+        import torch
+        from fusionlab.layers import SEModule
+        inputs = torch.normal(0, 1, (1, 256, 16, 16))
+        layer = SEModule(256)
+        outputs = layer(inputs)
+        assert list(outputs.shape) == [1, 256, 16, 16]
+
+        inputs = torch.normal(0, 1, (1, 256, 16, 16, 16))
+        layer = SEModule(256, spatial_dims=3)
+        outputs = layer(inputs)
+        assert list(outputs.shape) == [1, 256, 16, 16, 16]
