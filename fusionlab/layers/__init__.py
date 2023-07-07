554dc3f14eca95cf50c22da45814ea8b19ee94ce
CYLi <taipingeric@gmail.com>
2023-07-07 14:52:38 +0800

    [Add] ConvNormAct layer for N-Dims

--- fusionlab/layers/__init__.py
+++ fusionlab/layers/__init__.py
@@ -2,6 +2,7 @@ from fusionlab.configs import BACKEND
 if BACKEND == 'torch':
     from .squeeze_excitation.se import SEModule
     from .factories import *
+    from .base import ConvNormAct
 elif BACKEND == 'tf':
     from .squeeze_excitation.tfse import TFSEModule
 else:
---
+++ fusionlab/layers/base.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+from typing import Union, Sequence, Optional, Callable
+
+from fusionlab.layers import ConvND, BatchNorm
+
+class ConvNormAct(nn.Module):
+    def __init__(self, 
+        spatial_dims: int, 
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[Sequence[int], int],
+        stride: Union[Sequence[int], int] = 1,
+        padding: Union[Sequence[int], str] = 0,
+        dilation: Union[Sequence[int], int] = 1,
+        groups: int = 1,
+        bias: bool = True,
+        norm_layer: Optional[Callable[..., nn.Module]] = BatchNorm,
+        act_layer: Optional[Callable[..., nn.Module]] = nn.ReLU,
+        padding_mode: str = 'zeros',
+        inplace: Optional[bool] = bool,
+    ):
+        super().__init__()
+        self.conv = ConvND(
+            spatial_dims, 
+            in_channels, 
+            out_channels, 
+            kernel_size, 
+            stride, 
+            padding, 
+            dilation, 
+            groups, 
+            bias, 
+            padding_mode
+        )
+        self.norm = norm_layer(spatial_dims, out_channels)
+        params = {} if inplace is None else {"inplace": inplace}
+        self.act = act_layer(**params)
+        
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.act(x)
+        return x
